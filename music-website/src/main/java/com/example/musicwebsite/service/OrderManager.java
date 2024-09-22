e8291e692b451de740b947478c2c1263ee2f1528
asd asd <a1361683218@outlook.com>
2024-09-22 16:43:45 +0800

    1

--- music-website/src/main/java/com/example/musicwebsite/service/OrderManager.java
+++
@@ -1,7 +0,0 @@
-package com.example.musicwebsite.service;
-
-import com.example.musicwebsite.model.domain.Order;
-public interface OrderManager {
-    void sendPassword(Order order,String reciveAddress);
-    void sendCode(String code,String reciveAddress);
-}
